38fd412ea98398359c9e1f5f8b445bb10ebfeacc
mtotodev05 <mtotodev05@gmail.com>
2018-08-02 07:56:07 +0300

    post and get requests

--- 06_express_get_post/routes/users.js
+++ 06_express_get_post/routes/users.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Node Express App',condition:false });
+  res.send('respond with a resource');
 });
 
 module.exports = router;
